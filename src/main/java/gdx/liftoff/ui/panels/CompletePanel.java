cd5073804984371549e5fbaa20ee1c1e60da2797
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-05-29 20:29:08 -0700

    Show nice small message on success or failure.
    On failure, it should show an Exception's stacktrace and end with SETUP FAILED in red text. I haven't gotten it to fail yet, though.

--- src/main/java/gdx/liftoff/ui/panels/CompletePanel.java
+++ src/main/java/gdx/liftoff/ui/panels/CompletePanel.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Align;
 import gdx.liftoff.ui.LogoWidget;
 import gdx.liftoff.ui.UserData;
 
@@ -43,6 +44,7 @@ public class CompletePanel extends Table implements Panel {
         //output label
         Label outputLabel = new Label(UserData.log, skin, "descriptionWithMarkup");
         outputLabel.setWrap(true);
+        outputLabel.setAlignment(Align.center, Align.center);
         scrollTable.add(outputLabel).grow();
 
         //scroll to the bottom
